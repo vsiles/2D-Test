7b63212e5fbcd0cd7129334a119dd5946517d8b1
Vinz <vincent.siles@gmail.com>
2017-08-26 13:04:17 +0200

    update gitignore with win32 files + move to SDL2
    Previous commit was SDL1.2 only. Linux Makefile need update

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,85 @@
-#include <SDL/SDL.h>
+#include <iostream>
+#include <SDL2/SDL.h>
+
+using namespace std;
+
+static void myerror(char const *msg, SDL_Window *sdlWindow,
+				    SDL_Renderer *sdlRenderer)
+{
+	cerr << msg << SDL_GetError() << endl;
+	SDL_DestroyWindow(sdlWindow);
+	SDL_DestroyRenderer(sdlRenderer);
+	SDL_Quit();
+}
 
 int main(int argc, char *argv[])
 {
     (void)argc;
     (void)argv;
 
-    SDL_Surface* hello = NULL;
-    SDL_Surface* screen = NULL;
+#if 0
+	SDL_INIT_TIMER		timer subsystem
+	SDL_INIT_AUDIO		audio subsystem
+	SDL_INIT_VIDEO		video subsystem; automatically initializes the events subsystem
+	SDL_INIT_JOYSTICK	joystick subsystem; automatically initializes the events subsystem
+	SDL_INIT_HAPTIC		haptic(force feedback) subsystem
+	SDL_INIT_GAMECONTROLLER	controller subsystem; automatically initializes the joystick subsystem
+	SDL_INIT_EVENTS		events subsystem
+	SDL_INIT_EVERYTHING 		all of the above subsystems
+#endif
+	SDL_Init(SDL_INIT_EVERYTHING);
 
-    //Start SDL 
-    SDL_Init(SDL_INIT_EVERYTHING);
+	/* Create window & renderer */
+	SDL_Window *sdlWindow;
+	SDL_Renderer *sdlRenderer;
+	int ret = SDL_CreateWindowAndRenderer(0, 0, SDL_WINDOW_FULLSCREEN_DESKTOP, &sdlWindow, &sdlRenderer);
 
-    //Set up screen
-    screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
-    
+	if (ret) {
+		cerr << "SDL_CreateWindowAnRendere error: " << SDL_GetError() << endl;
+		SDL_Quit();
+		return -1;
+	}
+
+	/* Configure renderer ... */
+	SDL_SetRenderDrawColor(sdlRenderer, 0, 0, 0, 255);
+	SDL_RenderClear(sdlRenderer);
+	SDL_RenderPresent(sdlRenderer);
+
+	/* ... and scale the window to 640 x 480 */
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");  // make the scaled rendering look smoother.
+	SDL_RenderSetLogicalSize(sdlRenderer, 640, 480);
+
     //Load image
-    hello = SDL_LoadBMP("res/img/purple.bmp");
+    SDL_Surface *hello = SDL_LoadBMP("res/img/purple.bmp");
+	if (hello == NULL) {
+		myerror("SDL_LoadBMP error: ", sdlWindow, sdlRenderer);
+		return -1;
+	}
 
-    //Apply image to screen
-    SDL_BlitSurface(hello, NULL, screen, NULL);
-    
-    //Update Screen
-    SDL_Flip(screen);
-    
-    //Pause
+	SDL_Texture *texture = SDL_CreateTextureFromSurface(sdlRenderer, hello);
+	SDL_FreeSurface(hello);
+
+	if (texture == NULL) {
+		myerror("SDL_CreateTextureFromSurface error: ", sdlWindow, sdlRenderer);
+		return -1;
+	}
+	
+	ret = SDL_RenderCopy(sdlRenderer, texture, NULL, NULL);
+	if (ret) {
+		SDL_DestroyTexture(texture);
+		myerror("SDL_RenderCopy error: ", sdlWindow, sdlRenderer);
+	}
+
+	SDL_RenderPresent(sdlRenderer);
+
+    /* Pause to see the result */
     SDL_Delay(2000);
 
-    //Quit SDL
-    SDL_Quit();
+    /* Exit */
+	myerror("Success !", sdlWindow, sdlRenderer);
+
+	cout << "Input anything to quit" << endl;
+	cin >> ret;
 
     return 0;
 }
