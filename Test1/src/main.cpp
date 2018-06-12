80673b5f870ed67dc843100dc6c71e5b8760a258
Vincent Siles <vincent.siles@provenrun.com>
2018-06-12 17:28:10 +0200

    refactor texture loading and rendering.
    Lots of room for improvement

--- Test1/src/main.cpp
+++ Test1/src/main.cpp
@@ -12,11 +12,11 @@ struct gstate_t {
     SDL_Renderer *ren;
 };
 
-static int window_creation(gstate_t *gstate, const char* name,
-                           Uint32 renderer_flags,
-                           size_t posx = 100, size_t posy =  100,
-                           size_t width = 640, size_t height = 480,
-                           Uint32 window_flags = SDL_WINDOW_SHOWN)
+static int createWindow(gstate_t *gstate, const char* name,
+                        Uint32 renderer_flags,
+                        size_t posx = 100, size_t posy =  100,
+                        size_t width = 640, size_t height = 480,
+                        Uint32 window_flags = SDL_WINDOW_SHOWN)
 {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         logSDLError("SDL_Init");
@@ -27,16 +27,65 @@ static int window_creation(gstate_t *gstate, const char* name,
                                    window_flags);
     if (gstate->win == nullptr) {
         logSDLError("SDL_CreateWindow");
-	return -1;
+        return -1;
     }
 
     gstate->ren = SDL_CreateRenderer(gstate->win, -1, renderer_flags);
     if (gstate->ren == nullptr) {
         logSDLError("SDL_CreateRenderer");
-	return -1;
+        return -1;
     }
 
     return 0;
+}
+
+enum texture_kind {
+    TEXTURE_BMP = 0,
+};
+
+static SDL_Texture *createTexture(gstate_t *gstate, const char *path,
+                                  texture_kind kind = TEXTURE_BMP)
+{
+    SDL_Surface *surf = nullptr;
+
+    switch (kind) {
+        case TEXTURE_BMP:
+            surf = SDL_LoadBMP(path);
+            break;
+        default:
+            logError("Unsupported texture format");
+            return nullptr;
+    }
+
+    if (surf == nullptr) {
+        string msg = "createTexture loading - ";
+        msg = msg + path;
+        logSDLError(msg.c_str());
+        return nullptr;
+    }
+
+    SDL_Texture *tex = SDL_CreateTextureFromSurface(gstate->ren, surf);
+    SDL_FreeSurface(surf);
+
+    if (tex == nullptr) {
+        string msg = "createTexture to surface - ";
+        msg = msg + path;
+        logSDLError(msg.c_str());
+        return nullptr;
+    }
+
+    return tex;
+}
+
+static void renderTexture(gstate_t *gstate, SDL_Texture *tex, int x, int y)
+{
+    SDL_Rect dst;
+    dst.x = x;
+    dst.y = y;
+
+    /* TODO Optimize this */
+    SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
+    SDL_RenderCopy(gstate->ren, tex, NULL, &dst);
 }
 
 static int run(gstate_t *gstate)
@@ -44,32 +93,23 @@ static int run(gstate_t *gstate)
     int ret;
 
     /* TODO: make pos & size configurable */
-    ret = window_creation(gstate, "Hello World !",
-                          SDL_RENDERER_ACCELERATED |
-                          SDL_RENDERER_PRESENTVSYNC);
+    ret = createWindow(gstate, "Hello World !",
+                       SDL_RENDERER_ACCELERATED |
+                       SDL_RENDERER_PRESENTVSYNC);
     if (ret != 0)
         return ret;
 
     /* Load BMP */
     string imagePath = getResourcePath("img") + "hello.bmp";
-    SDL_Surface *bmp = SDL_LoadBMP(imagePath.c_str());
-    if (bmp == nullptr) {
-        logSDLError("SDL_LoadBMP");
-	return -1;
-    }
-
-    SDL_Texture *tex = SDL_CreateTextureFromSurface(gstate->ren, bmp);
-    SDL_FreeSurface(bmp);
-
+    SDL_Texture *tex = createTexture(gstate, imagePath.c_str());
     if (tex == nullptr) {
-        logSDLError("SDL_CreateTextureFromSurface");
         return -1;
     }
 
     /* Display BMP for 3 secs */
     for (int i = 0; i < 3; ++i) {
         SDL_RenderClear(gstate->ren);
-        SDL_RenderCopy(gstate->ren, tex, NULL, NULL);
+        renderTexture(gstate, tex, 0, 0);
         SDL_RenderPresent(gstate->ren);
         SDL_Delay(1000);
     }
@@ -77,7 +117,7 @@ static int run(gstate_t *gstate)
     SDL_DestroyTexture(tex);
 
     /* Exit */
-    log("Succes !");
+    log("Success !");
     return 0;
 }
 
