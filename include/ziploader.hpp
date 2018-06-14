0c035d64857129a97d595c974c909dfc922e7490
Vincent Siles <vincent.siles@ens-lyon.org>
2018-06-14 23:28:01 +0200

    Add lib fore shared code between tests

---
+++ include/ziploader.hpp
@@ -0,0 +1,6 @@
+#ifndef ZIP_H_INCLUDED
+#define ZIP_H_INCLUDED
+
+void zip_test(void);
+
+#endif /* ZIP_H_INCLUDED */
---
+++ lib/ziploader/ziploader.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+void zip_test(void)
+{
+    cout << "Testing zip library" << endl;
+}
